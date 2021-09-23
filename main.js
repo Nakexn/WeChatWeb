42a3260e02491cd761aa98b3f75125101852e442
那可心 <nakx@raysdata.com>
2021-09-23 13:01:34 +0800

    路由问题

--- main.js
+++ main.js
@@ -3,13 +3,13 @@ import { addClass, removeClass } from './util/index.js';
 
 const app = document.querySelector('#app');
 
-let currentRoute = '/';
+let currentRoute = '/wechat';
 
 let Page = routes[currentRoute];
 window.currentRoute = currentRoute;
 window.pageStack = [Page];
 
-// history.replaceState(null, null, currentRoute);
+history.replaceState(null, null, baseUrl + currentRoute);
 
 app.appendChild(Page);
 
