e7e49ce3107991dcccb1c1b7158bb00bdc314494
那可心 <nakx@raysdata.com>
2021-09-23 12:25:55 +0800

    路由问题

--- route/index.js
+++ route/index.js
@@ -1,25 +1,15 @@
 import WeChat from '../views/WeChat/index.js';
 import ChatDetail from '../views/ChatDetail/index.js';
 
-// export const baseUrl = window.location.origin;
-export const baseUrl = window.location.origin + '/WeChatWeb'; // 线上地址
-
-// export const routes = {
-//   '/': new WeChat().$el,
-//   '/wechat': new WeChat().$el,
-//   '/wechat/chat': new ChatDetail().$el,
-//   '/wechat/user_profile': 'UserProfile',
-//   '/address_book': 'AddressBook',
-//   '/moments': 'Moments',
-//   '/me': 'Me'
-// };
+export const baseUrl = window.location.origin;
+// export const baseUrl = window.location.origin + '/WeChatWeb'; // 线上地址
 
 export const routes = {
-  '/WeChatWeb': new WeChat().$el,
-  '/WeChatWeb/wechat': new WeChat().$el,
-  '/WeChatWeb/wechat/chat': new ChatDetail().$el,
-  '/WeChatWeb/wechat/user_profile': 'UserProfile',
-  '/WeChatWeb/address_book': 'AddressBook',
-  '/WeChatWeb/moments': 'Moments',
-  '/WeChatWeb/me': 'Me'
+  '/': new WeChat().$el,
+  '/wechat': new WeChat().$el,
+  '/wechat/chat': new ChatDetail().$el,
+  '/wechat/user_profile': 'UserProfile',
+  '/address_book': 'AddressBook',
+  '/moments': 'Moments',
+  '/me': 'Me'
 };
