4de9f08378e51a7164dadd16493d98a04ae61884
那可心 <nakx@raysdata.com>
2021-09-23 12:22:24 +0800

    线上路由

--- route/index.js
+++ route/index.js
@@ -4,12 +4,22 @@ import ChatDetail from '../views/ChatDetail/index.js';
 // export const baseUrl = window.location.origin;
 export const baseUrl = window.location.origin + '/WeChatWeb'; // 线上地址
 
+// export const routes = {
+//   '/': new WeChat().$el,
+//   '/wechat': new WeChat().$el,
+//   '/wechat/chat': new ChatDetail().$el,
+//   '/wechat/user_profile': 'UserProfile',
+//   '/address_book': 'AddressBook',
+//   '/moments': 'Moments',
+//   '/me': 'Me'
+// };
+
 export const routes = {
-  '/': new WeChat().$el,
-  '/wechat': new WeChat().$el,
-  '/wechat/chat': new ChatDetail().$el,
-  '/wechat/user_profile': 'UserProfile',
-  '/address_book': 'AddressBook',
-  '/moments': 'Moments',
-  '/me': 'Me'
+  '/WeChatWeb': new WeChat().$el,
+  '/WeChatWeb/wechat': new WeChat().$el,
+  '/WeChatWeb/wechat/chat': new ChatDetail().$el,
+  '/WeChatWeb/wechat/user_profile': 'UserProfile',
+  '/WeChatWeb/address_book': 'AddressBook',
+  '/WeChatWeb/moments': 'Moments',
+  '/WeChatWeb/me': 'Me'
 };
