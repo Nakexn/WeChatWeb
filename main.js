61874ff5478944b918acf1fdaaa864dd4489ff4b
那可心 <nakx@raysdata.com>
2021-10-11 01:06:30 +0800

    路由问题

--- main.js
+++ main.js
@@ -5,7 +5,8 @@ import Page from './components/Page.js';
 const app = document.querySelector('#app');
 const router = new Router({
   app,
-  routes
+  routes,
+  base: '/WeChatWeb'
 });
 
 window.$router = router;
