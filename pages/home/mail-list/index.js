069687c3ffc51252b9c220ae22add09f0827e4be
那可心 <nakx@raysdata.com>
2022-01-13 11:05:12 +0800

    去掉模版中多余内容

--- pages/home/mail-list/index.js
+++ pages/home/mail-list/index.js
@@ -30,34 +30,6 @@ class AddressBook extends Componet {
         </span>
       </div>
       <div class="content" style="height: 100%; background: #fff;">
-      </div>
-      <div class="footer">
-        <ul class="tab-bar">
-          <li class="item">
-            <a class="link router-link" data-link-to="/" href="javascript:;">
-              <i class="icon iconfont we-interactive"></i>
-              <span class="title">微信</span>
-            </a>
-          </li>
-          <li class="item">
-            <a class="link router-link selected" data-link-to="/address_book" href="javascript:;">
-              <i class="icon iconfont we-addressbook_fill"></i>
-              <span class="title">通讯录</span>
-            </a>
-          </li>
-          <li class="item">
-            <a class="link router-link" data-link-to="/find" href="javascript:;">
-              <i class="icon iconfont we-integral"></i>
-              <span class="title">发现</span>
-            </a>
-          </li>
-          <li class="item">
-            <a class="link router-link" data-link-to="/me" href="javascript:;">
-              <i class="icon iconfont we-mine"></i>
-              <span class="title">我</span>
-            </a>
-          </li>
-        </ul>
       </div>`;
     this.template = template;
     createDom(template);
